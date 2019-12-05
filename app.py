b7cbe65e7e42170270a43e70c48cb787b968ff2c
Poorna <poornaprag@gmail.com>
2019-12-04 19:14:56 -0800

    added graphs

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 import pickle
 import joblib
+import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
@@ -48,11 +49,18 @@ def knn(sample_test):
     result = loaded_model.predict(sample_test_dtm)
     return result
 
+
+
 def naive_bayes(sample_test):
     vect = joblib.load('vect1.pkl')
     loaded_model = joblib.load('nb.pkl')
+
     sample_test_dtm = vect.transform([sample_test])
     result = loaded_model.predict(sample_test_dtm)
+    pdx = pd.DataFrame(result)
+    what = pdx[0].value_counts().to_json(orient='records')
+    print('what is here')
+    print(what)
     return result
 
 
@@ -61,12 +69,13 @@ def result():
     if request.method == 'POST':
         sentence = request.form['text']
         print(sentence)
-        result = naive_bayes(sentence)
+        nb_result = naive_bayes(sentence)
+        asdf()
         knn_result = knn(sentence)
         svm_result = svm(sentence)
         # prediction = [nb_result,knn_result,svm_result]
         prediction = []
-        prediction.append(result)
+        prediction.append(nb_result)
         prediction.append(knn_result)
         prediction.append(svm_result)
         print((prediction))
@@ -105,7 +114,21 @@ def data():
 
 @app.route("/visualize")
 def vis():
-    return render_template('visual.html', title='Visualize')
+    df = pd.read_csv("comments_1.csv")
+    df1 = pd.read_csv("comments_2.csv")
+    df = df.append(df1)
+    print(df.columns)
+    df.columns=['x','y']
+    # print(df)
+    what = df['y'].value_counts()
+    print(what)
+
+    x=pd.DataFrame(what)
+    x['x']=['-1','0','1']
+    print(x)
+    result = x.to_json(orient='records')
+    # return result
+    return render_template('visual.html', title='Visualize',result=result)
 
 
 if __name__ == '__main__':
